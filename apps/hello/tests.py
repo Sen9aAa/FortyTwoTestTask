44a4428a63206ec9e65beaeb62c069e18ed8a38a
Sen9a <sen9a1990@gmail.com>
2016-11-03 23:19:41 +0200

    Test Teamplate

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.test import Client
 
 # Create your tests here.
 
@@ -7,3 +8,8 @@ class SomeTests(TestCase):
     def test_math(self):
         "put docstrings in your tests"
         assert(2 + 2 == 4)
+    
+    def test_template(self):
+    	self.client = Client()
+    	response = self.client.get('/')
+    	return self.assertEqual(response.status_code,200)
