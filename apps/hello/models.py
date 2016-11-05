e9efb2408ce3a7e1ee73729886c040f3e863b475
Sen9a <sen9a1990@gmail.com>
2016-11-05 14:56:58 +0200

    Create model MyInfo

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class MyInfo(models.Model):
+	name = models.CharField(max_length = 200)
+	surname = models.CharField(max_length = 200)
+	contacts = models.EmailField(max_length = 200)
+	bio = models.TextField(blank = True)
+	birthday = models.DateField()
+
