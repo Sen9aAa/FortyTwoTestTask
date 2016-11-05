b4605fa627cff767e9029931554633c8da22fa0a
Sen9a <sen9a1990@gmail.com>
2016-11-05 13:47:37 +0200

    Code the view, returning the rendered template, and the URL pattern directing to it

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+class HomeView(TemplateView):
+	template_name = 'base.html'
 
 
-# Create your views here.
--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-from django.views.generic.base import TemplateView
+from apps.hello.views import HomeView
 admin.autodiscover()
 
 
@@ -12,5 +12,5 @@ urlpatterns = patterns(
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$',TemplateView.as_view(template_name='base.html'),name='home'),
+    url(r'^$',HomeView.as_view(),name='home'),
 )
